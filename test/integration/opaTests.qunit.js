5cef09c1d16e725e592a1e1dd234c86e8b077cd0
rkranjan88 <120169300+rkranjan88@users.noreply.github.com>
2022-12-09 17:55:15 +0530

    Add files via upload

---
+++ test/integration/opaTests.qunit.js
@@ -0,0 +1,1 @@
+QUnit.config.autostart=false;sap.ui.getCore().attachInit(function(){"use strict";sap.ui.require(["com/mindset/analyzerdetail/AppAnalyzerDetail/test/integration/AllJourneys"],function(){QUnit.start()})});
---
+++ test/unit/AllTests.js
@@ -0,0 +1,1 @@
+sap.ui.define(["com/mindset/analyzerdetail/AppAnalyzerDetail/test/unit/controller/App.controller"],function(){"use strict"});
